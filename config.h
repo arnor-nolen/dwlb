a46c3b1a3e371f2bcf319f2bb37b7122562b23ab
Arnor Nolen <andrey1gorbulin@gmail.com>
2023-04-20 21:19:54 +0300

    Color scheme

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static bool bottom = false;
 // hide vacant tags
 static bool hide_vacant = false;
 // vertical pixel padding above and below text
-static uint32_t vertical_padding = 5;
+static uint32_t vertical_padding = 2;
 // allow in-line color commands in status text
 static bool status_commands = true;
 // center title text
@@ -24,9 +24,9 @@ static char *tags_noipc[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 // set 16-bit colors for bar
 // 8-bit color can be converted to 16-bit color by simply duplicating values e.g
 // 0x55 -> 0x5555, 0xf1 -> 0xf1f1
-static pixman_color_t active_fg_color = { .red = 0xeeee, .green = 0xeeee, .blue = 0xeeee, .alpha = 0xffff, };
-static pixman_color_t active_bg_color = { .red = 0x0000, .green = 0x5555, .blue = 0x7777, .alpha = 0xffff, };
-static pixman_color_t inactive_fg_color = { .red = 0xbbbb, .green = 0xbbbb, .blue = 0xbbbb, .alpha = 0xffff, };
-static pixman_color_t inactive_bg_color = { .red = 0x2222, .green = 0x2222, .blue = 0x2222, .alpha = 0xffff, };
+static pixman_color_t active_fg_color = { .red = 0x2828, .green = 0x2828, .blue = 0x2828, .alpha = 0xffff, };
+static pixman_color_t active_bg_color = { .red = 0x8383, .green = 0xa5a5, .blue = 0x9898, .alpha = 0xffff, };
+static pixman_color_t inactive_fg_color = { .red = 0xebeb, .green = 0xdbdb, .blue = 0xb2b2, .alpha = 0xffff, };
+static pixman_color_t inactive_bg_color = { .red = 0x2828, .green = 0x2828, .blue = 0x2828, .alpha = 0xffff, };
 static pixman_color_t urgent_fg_color = { .red = 0x2222, .green = 0x2222, .blue = 0x2222, .alpha = 0xffff, };
 static pixman_color_t urgent_bg_color = { .red = 0xeeee, .green = 0xeeee, .blue = 0xeeee, .alpha = 0xffff, };
